bde052f246cd584e5e7ccd9b18e04b13939f72ef
Takanori Kotama <104612339+kotama7@users.noreply.github.com>
2025-06-20 18:52:25 +0900

    Add InterestGraph component

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,6 +2,7 @@ import Home from './components/home/home';
 import Header from './components/header/header';
 import BioTree from './components/bio/BioTree';
 import SkillTree from './components/skills/SkillTree';
+import InterestGraph from './components/interests/InterestGraph';
 
 import { useState } from 'react';
 
@@ -23,6 +24,7 @@ function App() {
       />
       <BioTree />
       <SkillTree />
+      <InterestGraph />
     </>
   );
 }

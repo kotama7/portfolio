c1cdfefcca0f9a054bdd63a8431334cf61fa5902
Takanori Kotama <104612339+kotama7@users.noreply.github.com>
2025-06-21 21:36:31 +0900

    feat: add language switch component

--- portfolio/src/components/header/header.tsx
+++ portfolio/src/components/header/header.tsx
@@ -2,22 +2,14 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import LanguageSwitch from '../LanguageSwitch';
 
 interface HeaderProps {
     lang: string;
     setLang: (lang: string) => void;
 }
+
 export default function Header(props: HeaderProps){
-
-    const changeLanguage = () => {
-        if (props.lang === 'en') {
-            props.setLang('ja');
-        } else {
-            props.setLang('en');
-        }
-    }    
-
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -25,9 +17,7 @@ export default function Header(props: HeaderProps){
                     <Typography variant="h6" color="inherit" component="div" sx={{ flexGrow: 1 }}>
                         {props.lang === 'en' ? 'Takanori Kotama' : '樹神 宇徳'}
                     </Typography>
-                    <Button color="inherit" onClick={changeLanguage}>
-                        {props.lang === 'en' ? '日本語' : 'English'}
-                    </Button>
+                    <LanguageSwitch lang={props.lang} setLang={props.setLang} />
                 </Toolbar>
             </AppBar>
         </Box>
